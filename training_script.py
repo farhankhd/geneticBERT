2bfa7786a58674a5277b73e84e388948bfe69681
rohola <hilbert.cantor@gmail.com>
2023-05-03 14:50:28 -0600

    added save and load checkpoints

--- training_script.py
+++ training_script.py
@@ -3,6 +3,7 @@ import torch
 from transformers import DataCollatorForLanguageModeling, BertConfig, BertForMaskedLM
 from transformers import PreTrainedTokenizer
 import torch.optim as optim
+import os
 
 
 class GeneticDataset(Dataset):
@@ -81,24 +82,54 @@ class GeneTokenizer(PreTrainedTokenizer):
         return len(self.vocab)
 
 
+def save_checkpoint(model, optimizer, epoch, output_dir):
+    checkpoint_dir = os.path.join(output_dir, f"checkpoint-{epoch}")
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    model.save_pretrained(checkpoint_dir)
+
+    optimizer_path = os.path.join(checkpoint_dir, "optimizer.pt")
+    torch.save(optimizer.state_dict(), optimizer_path)
+
+    print(f"Checkpoint saved at {checkpoint_dir}")
+
+
+def load_checkpoint(checkpoint_dir, model, optimizer):
+    model = model.from_pretrained(checkpoint_dir)
+
+    optimizer_path = os.path.join(checkpoint_dir, "optimizer.pt")
+    optimizer.load_state_dict(torch.load(optimizer_path))
+
+    return model, optimizer
+
+def evaluate(model, eval_dataloader, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for batch in eval_dataloader:
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+            total_loss += loss.item()
+
+    avg_loss = total_loss / len(eval_dataloader)
+    return avg_loss
+
+
 # sample vocab
 
 vocab = [item.rstrip() for item in open("gene_data/gene_vocab.txt").readlines()]
 tokenizer = GeneTokenizer(vocab)
 
-# input = 'ABHD2, ANKFN1, APOC4-APOC2'
-#
-# tokenizer = GeneTokenizer(vocab)
-# x = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
-# print(x)
-# print(tokenizer.tokenize(input))
-# print(tokenizer.convert_tokens_to_ids(tokenizer.tokenize(input)))
+train_path = "gene_data/train.txt"
+train_dataset = GeneticDataset(train_path, tokenizer, 512)
 
-sample_input_path = "gene_data/train.txt"
-dataset = GeneticDataset(sample_input_path, tokenizer, 512)
-# for sample in dataset:
-#     print(sample)
+eval_path = "gene_data/eval.txt"
+eval_dataset = GeneticDataset(eval_path, tokenizer, 512)
 
+output_dir = "gene_data/checkpoints"
 
 # Set up the data collator
 data_collator = DataCollatorForLanguageModeling(
@@ -108,10 +139,17 @@ data_collator = DataCollatorForLanguageModeling(
 )
 
 # Set up the DataLoader
-dataloader = DataLoader(
-    dataset,
+train_dataloader = DataLoader(
+    train_dataset,
     batch_size=2,
     collate_fn=data_collator
+)
+
+eval_dataloader = DataLoader(
+    eval_dataset,
+    batch_size=2,
+    collate_fn=data_collator,
+    shuffle=False
 )
 
 # Set up the model
@@ -131,7 +169,7 @@ for epoch in range(epochs):
     print(f"Epoch {epoch + 1}/{epochs}")
 
     model.train()
-    for i, batch in enumerate(dataloader):
+    for i, batch in enumerate(train_dataloader):
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
         labels = batch['labels'].to(device)
@@ -145,3 +183,10 @@ for epoch in range(epochs):
         optimizer.step()
 
         print(f"Batch {i + 1} - Loss: {loss.item()}")
+
+    # Save checkpoint
+    save_checkpoint(model, optimizer, epoch + 1, output_dir)
+
+    # Evaluate
+    eval_loss = evaluate(model, eval_dataloader, device)
+    print(f"Epoch {epoch + 1} - Evaluation Loss: {eval_loss}")
